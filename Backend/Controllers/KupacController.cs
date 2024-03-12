705e7a4d697fd2810049ceced01fd0634375dfba
lcrljic <lukacrljic7@gmail.com>
2024-03-12 19:21:05 +0100

    Završni zadatak

---
+++ Backend/Controllers/KupacController.cs
@@ -0,0 +1,67 @@
+﻿using EdunovaAPP.Data;
+using EdunovaAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EdunovaAPP.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class KupacController
+    {
+        // Dependency injection
+        // Definiraš privatno svojstvo
+        private readonly EdunovaContext _context;
+
+        // Dependency injection
+        // U konstruktoru primir instancu i dodjeliš privatnom svojstvu
+        public KupacController(EdunovaContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(_context.Kupci.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Kupac smjer)
+        {
+            _context.Kupci.Add(smjer);
+            _context.SaveChanges();
+            return new JsonResult(smjer);
+        }
+
+        [HttpPut]
+        [Route("{sifra:int}")]
+        public IActionResult Put(int sifra, Kupac smjer)
+        {
+            var smjerIzBaze = _context.Kupci.Find(sifra);
+            // za sada ručno, kasnije će doći Mapper
+            smjerIzBaze.Ime = smjer.Ime;
+            smjerIzBaze.Prezime= smjer.Prezime;
+            smjerIzBaze.Mjesto= smjer.Mjesto;
+            smjerIzBaze.UlicaIBroj=smjer.UlicaIBroj;
+            smjerIzBaze.BrojMobitela=smjer.BrojMobitela;
+
+            _context.Kupci.Update(smjerIzBaze);
+            _context.SaveChanges();
+
+            return new JsonResult(smjerIzBaze);
+        }
+
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        [Produces("application/json")]
+        public IActionResult Delete(int sifra)
+        {
+            var smjerIzBaze = _context.Kupci.Find(sifra);
+            _context.Kupci.Remove(smjerIzBaze);
+            _context.SaveChanges();
+            return new JsonResult(new { poruka="Obrisano"});
+        }
+
+    }
+}
---
+++ Backend/Models/Kupac.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EdunovaAPP.Models
+{
+    public class Kupac: Entitet
+    {
+        public string? Ime { get; set; }
+        public string? Prezime { get; set; }
+        public string? Mjesto { get; set; }
+        public string? UlicaIBroj { get; set; }
+        public string? BrojMobitela { get; set; }
+
+    }
+}
